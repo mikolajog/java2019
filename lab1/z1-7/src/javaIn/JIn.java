07172337da3f49294deaa4d96de5eee72582624c
Mikolaj <mikiogarek@gmail.com>
2018-10-07 22:25:47 +0200

    Zadania 1-7

---
+++ lab1/z1-7/src/javaIn/JIn.java
@@ -0,0 +1,32 @@
+package javaIn;
+
+import java.io.*;
+
+public class JIn {
+    public static String getString() {
+        String text = null;
+        try {
+            InputStreamReader rd = new InputStreamReader(System.in);
+            BufferedReader bfr = new BufferedReader(rd);
+
+            text = bfr.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return text;
+    }
+
+    public static int getNumber() {
+        int text = 0;
+        try {
+            InputStreamReader rd = new InputStreamReader(System.in);
+            BufferedReader bfr = new BufferedReader(rd);
+
+            text = Integer.parseInt(bfr.readLine());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return text;
+    }
+
+}
---
+++ lab1/z1-7/src/liczbyPierwsze/LiczbyPierwszeProgram.java
@@ -0,0 +1,11 @@
+package liczbyPierwsze;
+
+import javaIn.JIn;
+
+public class LiczbyPierwszeProgram {
+    public static void main (String[] argv){
+        LiczbyPierwsze p1 = new LiczbyPierwsze();
+        p1.wczytaj();
+        p1.znajdzLiczbyPierwsze();
+    }
+}
