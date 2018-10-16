71862d260a70dd28b519ab1852a70ef1e197e193
Mikolaj <mikiogarek@gmail.com>
2018-10-16 15:56:19 +0200

    DataFrame

--- lab2/src/dataframe/DataFrame.java
+++ lab2/src/dataframe/DataFrame.java
@@ -1,29 +1,7 @@
 package dataframe;
 
-import javax.xml.crypto.Data;
 import java.util.ArrayList;
-
-/*
-
-Zaimplementuj klasę DataFrame, która pozwalać będzie na przechowywanie dowolnej ilości danych, dowolnego typu w formacie kolumnowym (analogicznie jak w bazie danych).
- Konstruktor tej klasy powinien przyjmować jako parametry listę stringów, określającą nazwy kolumn, oraz listę typów określającą typy poszczególnych kolumn:
-
-DataFrame df = new  DataFrame(new String[]{"kol1","kol2","kol3"},
-                         new String[]{"int","double","MyCustomType"}
-
-Wymagania dodatkowe:
-
-    Kolumny powinny być przechowywane jako ArrayList
-
-    Klasa powinna posiadać metody:
-        size() – zwracającą ilość wierszy w całej DF (uwaga – DF nie może mieć jednej z kolumn dłuższej niż pozostałe
-        get(String colname) – zwracającą kolumnę o podanej nazwie
-        get(String [] cols, boolean copy) – zwracającą nową DataFrame z kolumnami podanymi jako parametry. W zależności od wartości parametru copy albo tworzona jest głęboka kopia, albo płytka.
-        iloc(int i) – zwracającą wiersz o podanym indeksie (jako nową DataFrame)
-        iloc(int from, int to) – zwracającą nową DataFrame z wierszami z podanego zakresu
-
-*/
-
+import java.util.Arrays;
 
 
 public class DataFrame {
@@ -46,7 +24,10 @@ public class DataFrame {
         }
     }
 
+    public DataFrame(){}
+
     public int size(){
+
         return list.get(0).size();
     }
 
@@ -72,27 +53,39 @@ public class DataFrame {
 
     public DataFrame get(String[] cols, boolean copy)
     {
-        if(copy)
-        {
-
-        }
-        else
-        {
-            String[] empty = new String[]{};
-            DataFrame d1 = new DataFrame(empty, empty);
-            for(int i=0; i<cols.length; i++)
+        DataFrame d1 = new DataFrame();
+        d1.types = new String[cols.length];
+        d1.columns_names = new String[cols.length];
+        d1.columns_names = cols.clone();
+        int i=0;
+        for(String x:cols){
+            if(copy)
             {
-                ArrayList<Object> a1 = new ArrayList<>();
-                if(cols[i].equals(types[i]))
+                d1.types[i] = types[Arrays.asList(columns_names).indexOf(x)];
+
+                if(list.get(Arrays.asList(columns_names).indexOf(x)).get(0).getClass().getSimpleName().equals("MyCustomType"))
                 {
-                    for(int j=0; j<list.get(i).size(); j++)
-                    {
-                        a1.add(list.get(i).get(j));
+                    d1.list.add(new ArrayList<>());
+
+                    for(int j=0; j<list.get(Arrays.asList(columns_names).indexOf(x)).size(); j++){
+                        MyCustomType t1 = new MyCustomType((MyCustomType)(list.get(Arrays.asList(columns_names).indexOf(x)).get(j)));
+                        d1.list.get(d1.list.size()-1).add(t1);
                     }
-                    d1.list.add(a1);
+                }
+                else{
+                    d1.list.add(new ArrayList<>(list.get(Arrays.asList(columns_names).indexOf(x))));
                 }
             }
+            else{
+                d1.types[i] = types[Arrays.asList(columns_names).indexOf(x)];
+                d1.list.add(new ArrayList<>(list.get(Arrays.asList(columns_names).indexOf(x))));
+            }
+            i++;
         }
+
+        return d1;
+
+
     }
 
     public DataFrame iloc(int i)
@@ -130,21 +123,52 @@ public class DataFrame {
     public static void main(String[] args)
     {
         DataFrame df = new  DataFrame(new String[]{"kol1","kol2","kol3"}, new String[]{"int","double","MyCustomType"});
-        String variable = "zmienna";
-        df.add_line(new Object[]{1, 2.3, "cos"});
-        df.add_line(new Object[]{2, 3.3, "cos1"});
-        df.add_line(new Object[]{3, 4.3, "cos2"});
+        MyCustomType variable = new MyCustomType(1,2);
+        df.add_line(new Object[]{1, 2.3, variable});
+        df.add_line(new Object[]{2, 3.3, variable});
+        df.add_line(new Object[]{3, 4.3, variable});
 
-        System.out.println("DataFrame size is: "+df.size());
-        DataFrame d2 = df.iloc(0);
-        System.out.println(d2.list.get(0).get(0)+" "+d2.list.get(1).get(0)+" "+d2.list.get(2).get(0));
 
-        DataFrame d3 = df.iloc(1,2);
+       System.out.println("DataFrame size is: "+df.size());
+
+       System.out.println("Sprawdzam iloc(0)");
+       DataFrame d2 = df.iloc(0);
+       System.out.println(d2.list.get(0).get(0)+" "+d2.list.get(1).get(0)+" "+d2.list.get(2).get(0));
+
+        System.out.println("Sprawdzam iloc(0,1)");
+        DataFrame d3 = df.iloc(0,1);
         System.out.println(d3.list.get(0).get(0)+" "+d3.list.get(1).get(0)+" "+d3.list.get(2).get(0));
         System.out.println(d3.list.get(0).get(1)+" "+d3.list.get(1).get(1)+" "+d3.list.get(2).get(1));
 
+        System.out.println("Sprawdzam get(kol1)");
         ArrayList<Object> a1 = df.get("kol1");
         System.out.println(a1.get(0)+" "+ a1.get(1)+ " "+a1.get(2));
+
+        System.out.println("Sprawdzam get(kol2)");
+        ArrayList<Object> a2 = df.get("kol2");
+        System.out.println(a2.get(0)+" "+ a2.get(1)+ " "+a2.get(2));
+
+
+        DataFrame d4 = df.get(new String[]{"kol2", "kol3"}, true);
+        System.out.println(d4.list.get(0).get(0)+" "+d4.list.get(1).get(0));
+
+        ArrayList<Object> x = df.get("kol3");
+        MyCustomType m = (MyCustomType) x.get(0);
+        System.out.println(m.getA());
+        System.out.println(m.getB());
+
+        ArrayList<Object> x1 = d4.get("kol3");
+        MyCustomType m1 = (MyCustomType) x1.get(0);
+        System.out.println(m1.getA());
+        System.out.println(m1.getB());
+
+        m.setA(100);
+        m.setB(200);
+        System.out.print(m.getA()+" ");
+        System.out.println(m.getB());
+        System.out.print(m1.getA()+" ");
+        System.out.println(m1.getB());
+
 
     }
 }
